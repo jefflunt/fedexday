04fa96605a4855cee5669819bf6d6a9305429130
Jeff Lunt <jefflunt@gmail.com>
2012-01-17 20:33:51 -0600

    References #3 - Corrected an issue with the long-expired scope. It will now delete any scratched ideas over 1 week old, or ANY idea that hasn't been touched in 9 months.

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -5,5 +5,5 @@ class Idea < ActiveRecord::Base
   validates :title, :presence => true
   validates :body, :presence => true
   
-  scope :long_expired, where("updated_at < '#{9.months.ago}'")
+  scope :long_expired, where("(updated_at < '#{1.week.ago}' AND scratched = 't') OR (updated_at < '#{9.months.ago}')")
 end
