3720e75ef0a4ced6c74dc7611df4531695ca7e93
Jeff Lunt <jefflunt@gmail.com>
2012-01-17 19:46:17 -0600

    Closes #3 - Removed all unnecessary actions, and views.

--- app/controllers/endorsements_controller.rb
+++ app/controllers/endorsements_controller.rb
@@ -1,42 +1,4 @@
 class EndorsementsController < ApplicationController
-  # GET /endorsements
-  # GET /endorsements.json
-  def index
-    @endorsements = Endorsement.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @endorsements }
-    end
-  end
-
-  # GET /endorsements/1
-  # GET /endorsements/1.json
-  def show
-    @endorsement = Endorsement.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @endorsement }
-    end
-  end
-
-  # GET /endorsements/new
-  # GET /endorsements/new.json
-  def new
-    @endorsement = Endorsement.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @endorsement }
-    end
-  end
-
-  # GET /endorsements/1/edit
-  def edit
-    @endorsement = Endorsement.find(params[:id])
-  end
-
   # POST /endorsements
   # POST /endorsements.json
   def create
@@ -46,31 +8,13 @@ class EndorsementsController < ApplicationController
     redirect_to @endorsement.idea
   end
 
-  # PUT /endorsements/1
-  # PUT /endorsements/1.json
-  def update
-    @endorsement = Endorsement.find(params[:id])
-
-    respond_to do |format|
-      if @endorsement.update_attributes(params[:endorsement])
-        format.html { redirect_to @endorsement, :notice => 'Endorsement was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.json { render :json => @endorsement.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   # DELETE /endorsements/1
   # DELETE /endorsements/1.json
   def destroy
     @endorsement = Endorsement.find(params[:id])
+    idea = @endorsement.idea
     @endorsement.destroy
 
-    respond_to do |format|
-      format.html { redirect_to endorsements_url }
-      format.json { head :ok }
-    end
+    redirect_to idea
   end
 end
